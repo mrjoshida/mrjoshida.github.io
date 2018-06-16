3f95385a970301bcd0152ed60a5bb8baebfca63a
Josh Caldwell <josh@code.org>
2018-06-16 06:09:27 -0700

    start playing with effects

--- examples/sound-visualizer-fx.js
+++ examples/sound-visualizer-fx.js
@@ -1,8 +1,3 @@
-//
-// This could be interesting using Sprite Lab behaviors, eg
-// <sprite> starts <rotating> to <bass>
-//
-
 var pieces, radius, fft, mapMouseX, mapMouseY, toggleBtn, audio, uploadBtn, uploadedAudio, uploadAnim;
 
 function preload() {
@@ -32,38 +27,37 @@ function setup() {
 
 function draw() {
 
+    //
+    // I'm imagining the API here might look something like
+    // setSides(bassEnergy);
+    // setColor(trebleEnergy);
+    // setSize(midEnergy);
+    // and we would take care of scaling values behind the scenes.
+    //
+
 
 	background("white");
 
+	noFill();
 
-	noStroke();
-
-	var spectrum = fft.analyze();
-
-
-    for (var i = 0; i < spectrum.length; i++) {
-        var x = map(i, 0, spectrum.length, 0, width);
-        var h = -height + map(spectrum[i], 0, 255, height, 0);
-        fill(spectrum[i], 255, 255, 127)
-        rect(x, height, width/spectrum.length, h);
-    }
+	fft.analyze();
 
 	var bass = fft.getEnergy("bass");
 	var treble = fft.getEnergy("treble");
 	var mid = fft.getEnergy("mid");
 
 	var posMid = map(mid, 0, 255, 200, 50);
-	var scaleMid = map(mid, 0, 255, .25, 2);
+	var scaleMid = map(mid, 0, 255, .5, 1.5);
 	var sidesMid = map(mid, 0, 255, 3, 12);
 	var rotationMid = map(mid, 0, 255, -180, 180);
 
 	var posTreble = map(treble, 0, 255, 200, 50);
-	var scaleTreble = map(treble, 0, 255, .25, 2);
+	var scaleTreble = map(treble, 0, 255, .5, 1.5);
 	var sidesTreble = map(treble, 0, 5, 20);
 	var rotationTreble = map(treble, 0, 255, -180, 180);
 
 	var posBass = map(bass, 0, 255, 200, 50);
-	var scaleBass = map(bass, 0, 255, .25, 2);
+	var scaleBass = map(bass, 0, 255, .5, 1.5);
 	var sidesBass = map(bass, 0, 255, 3, 12);
 	var rotationBass = map(bass, 0, 255, -180, 180);
 
