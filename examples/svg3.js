f2e56102d13d854ea66591f96eebdace7b8498e9
Josh Caldwell <josh@code.org>
2018-10-22 20:17:35 -0700

    Update p5.play to include `imageElement`

---
+++ examples/svg3.js
@@ -0,0 +1,24 @@
+// Pure SVG animations
+// Currently p5play animations don't allow for svg
+// so drawing them manually
+
+var images = [];
+
+function preload() {
+    for (var i=0; i<24; i++) {
+        // images.push(loadImageElement('assets/svg/bear' + i + '.svg')); // Direct export from bodymovin
+        images.push(loadImageElement('assets/svgo/bear' + i + '.svg')); // Optimized with SVGO
+    }
+    console.log(images)
+}
+
+function setup() {
+  createCanvas(400, 400);
+  frameRate(24);
+}
+
+function draw() {
+  background(255, 255, 255);
+  for (var i = 63; i>=0; i--)
+  imageElement(images[frameCount % 24], (i % 8) * 50, Math.floor(i / 8) * 50, mouseX, mouseX);
+}
